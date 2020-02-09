e4362aebe75846e67c20cb4937743091018601f7
Pedja <vpedja12@gmail.com>
2020-02-08 23:21:23 -0800

    table finished

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from "react";
+import API from "../src/utils/API"
+
+import Table from "../src/components/Table";
+import "./App.css";
+
+// const Picture = ({ values }) => {
+//   return (
+//     <>
+//       {values.map((image) => {
+//         return (
+//           <img src={image} alt={image} className="image"/>
+//         )
+//       })}
+//     </>
+//   );
+// };
 
 function App() {
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Employees",
+        columns: [
+          // {
+          //   Header: "Picture",
+          //   accessor: "image",
+          // Cell: ({cell: {value}}) => { return <div><Picture values={value}/></div> }
+          // },
+          {
+            Header: "First Name",
+            accessor: "firstName"
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName"
+          }
+        ]
+      },
+      {
+        Header: "Details",
+        columns: [
+          {
+            Header: "Username",
+            accessor: "username"
+          },
+          {
+            Header: "City",
+            accessor: "city"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
+  const [data, setData] = useState([]);
+  
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
+  //     setData(result.data);
+  //     console.log(result.data);
+  //   })();
+  // }, []);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  function loadUsers() {
+    API.searchTerms()
+      .then(res => {
+        setData(res);
+        console.log(res);
+        });
+      }
+
+  console.log(data);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
---
+++ employeedirectory/src/components/Table/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useTable, useFilters, useSortBy } from "react-table";
+import "./style.css";
+
+export default function Table({ columns, data }) {
+  const [filterInput, setFilterInput] = useState("");
+
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useFilters,
+    useSortBy
+  );
+
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("firstName", value);
+    setFilterInput(value);
+  };
+
+  // Render the UI for your table
+  return (
+    <>
+    <h1 className="titleNav">Employee Directory</h1>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search employee"}
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={
+                    column.isSorted
+                      ? column.isSortedDesc
+                        ? "sort-desc"
+                        : "sort-asc"
+                      : ""
+                  }
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
