b97a029778072f90059b7486e74bd1009bbcbd80
Pedja <vpedja12@gmail.com>
2020-03-13 23:51:00 -0700

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000;
-
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
